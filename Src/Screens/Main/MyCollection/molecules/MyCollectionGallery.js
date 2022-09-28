eac607f6f884a7bb4f6bcb6447f395051fc90e15
ZAIDANWAR22 <za05404@gmail.com>
2022-09-29 01:08:04 +0500

    MyCollectionGallery

--- Src/Screens/Main/MyCollection/molecules/MyCollectionGallery.js
+++ Src/Screens/Main/MyCollection/molecules/MyCollectionGallery.js
@@ -1,14 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import gallery from "../../../../../Assets/Gallery";
 
 const MyCollectionGallery = () => {
+//   const Images = [
+//     { id: 1, uri: require(gallery.gallery01) },
+//     { id: 2, uri: require(gallery.gallery02) },
+//     { id: 3, uri: require(gallery.gallery03) },
+//   ];
   return (
     <View>
-      <View>
-        
-      </View>
-    </View>
-  )
-}
+      {/* <View> */}
 
-export default MyCollectionGallery
+      {/* {Images.map((image) => (
+        <View> 
+            <Image source={image.uri} />
+        </View>
+      ))} */}
+      <Text>Hello</Text>
+
+      {/* </View> */}
+    </View>
+  );
+};
+
+export default MyCollectionGallery;
