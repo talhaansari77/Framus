3efe5079ff59ffb43433f04f26d708cd533b0a63
ZAIDANWAR22 <za05404@gmail.com>
2022-09-29 01:15:10 +0500

    Changes

--- Src/Screens/Main/MyCollection/molecules/MyCollectionGallery.js
+++ Src/Screens/Main/MyCollection/molecules/MyCollectionGallery.js
@@ -3,23 +3,15 @@ import React from "react";
 import gallery from "../../../../../Assets/Gallery";
 
 const MyCollectionGallery = () => {
-//   const Images = [
-//     { id: 1, uri: require(gallery.gallery01) },
-//     { id: 2, uri: require(gallery.gallery02) },
-//     { id: 3, uri: require(gallery.gallery03) },
-//   ];
+
   return (
     <View>
-      {/* <View> */}
 
-      {/* {Images.map((image) => (
-        <View> 
-            <Image source={image.uri} />
-        </View>
-      ))} */}
+
+
       <Text>Hello</Text>
 
-      {/* </View> */}
+
     </View>
   );
 };
